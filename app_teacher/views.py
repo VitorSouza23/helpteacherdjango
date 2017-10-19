f872f72d3f808c1e567135f4027b77f288c5cd0f
vitorsaouza23 <vitordesolza@gmail.com>
2017-10-19 07:24:12 -0200

    novas alterações

--- app_teacher/views.py
+++ app_teacher/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+import models
+
 # Create your views here.
 def teacher(request):
-    my_dict = {}
+    my_dict = {"professores", models.TeacherVO.objects.all()}
     return render(request, 'app_teacher/teacher.html', context=my_dict)
