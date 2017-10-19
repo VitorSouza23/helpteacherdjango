ca78914f8c641192722fbd1cb1c2ca50aca9322b
vitorsaouza23 <vitordesolza@gmail.com>
2017-10-19 07:48:35 -0200

    Agora com bd e migrações

--- app_teacher/views.py
+++ app_teacher/views.py
@@ -7,5 +7,5 @@ import models
 
 # Create your views here.
 def teacher(request):
-    my_dict = {"professores", models.TeacherVO.objects.all()}
+    my_dict = {"professores": models.TeacherVO.objects.all()}
     return render(request, 'app_teacher/teacher.html', context=my_dict)
