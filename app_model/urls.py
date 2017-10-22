f7f6e28ed6a5011315884ea16e94649467f24048
Arnaldo Madeira <arnaldomadeira11@hotmail.com>
2017-10-22 00:57:50 -0200

    Atualizando design e links

--- app_model/urls.py
+++ app_model/urls.py
@@ -3,7 +3,7 @@ import views
 
 urlpatterns = [
     url(r'^grades/', views.grades, name='grades'),
-    url(r'^classes/', views.classes, name='grades'),
-    url(r'^courses/', views.courses, name='grades'),
-    url(r'^students/', views.students, name='grades'),
+    url(r'^classes/', views.classes, name='classes'),
+    url(r'^courses/', views.courses, name='courses'),
+    url(r'^students/', views.students, name='students'),
 ]
