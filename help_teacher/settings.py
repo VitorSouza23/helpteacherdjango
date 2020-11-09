d6f0fcfd09f3c4afb34f46cee5e1d64cfe89ed92
Vitor de Souza <vitordesolza@gmail.com>
2020-11-09 17:26:04 -0300

    Alterações pendentes

--- help_teacher/settings.py
+++ help_teacher/settings.py
@@ -21,7 +21,7 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@=$4_hquoz9gfkg#79h*k)04oj$jbt3d)9c)pyv76u1#4=hz=p'
+SECRET_KEY = '???????'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
